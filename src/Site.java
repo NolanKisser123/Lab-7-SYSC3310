e5b5e4fc5f917a0309bd9e8935bb8e1cf8582ab9
Nolan <nolankisser@cmail.carleton.ca>
2023-11-13 12:56:26 -0500

    Added Comments

--- src/Site.java
+++ src/Site.java
@@ -1,12 +1,30 @@
 public abstract class Site {
     public final double TAX_RATE = 0.15;
+
+    /**
+     * Class for calculating the bill amount
+     * @param units
+     * @param rate
+     * @return
+     */
     public double getBillableAmount(int units, double rate) {
         double base = getBaseAmount(units, rate);
         double tax = getTaxAmount(base);
         return base + tax;
     }
 
+    /**
+     * Abstract class for calculating base amount
+     * @param units
+     * @param rate
+     * @return
+     */
     public abstract double getBaseAmount(int units, double rate);
 
+    /**
+     * Abstract class for calculating tax amount
+     * @param base
+     * @return
+     */
     public abstract double getTaxAmount(double base);
 }
